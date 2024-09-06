3fe755d548bf9149d95090a9abfe5b13f5cda639
Daryl B. Arias <yasoarias@gmail.com>
2024-09-06 18:39:41 +0800

    Delete phpCORSerror directory

--- phpCORSerror/backend/api/getEvent.php
+++
@@ -1,9 +0,0 @@
-<?php
-require 'db.php';
-
-$sql = "SELECT * FROM events";
-$stmt = $pdo->query($sql);
-$events = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-echo json_encode($events);
-?>
--- phpCORSerror/frontend/vue.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  devServer: {
-    proxy: "http://localhost/ems-test",
-  },
-};
